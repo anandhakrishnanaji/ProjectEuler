d27a7c9ade3bb76f34b1c02cade8061e197d74a4
A Anandha Krishnan <creattech2000@gmail.com>
2024-03-25 23:43:08 +0530

    Fixed the naming of solutions

--- problem_10.py
+++ problem_10.py
@@ -1,20 +1,16 @@
-MAX_SIZE = 200_000
+MAX_SIZE = 2_000_000
 
 status = [
     True,
 ] * MAX_SIZE
 
 status[0] = status[1] = False
-count = 0
-prime_10001 = 0
+count_sum = 0
 
 for i in range(2, MAX_SIZE):
     if status[i]:
-        count += 1
-        if count == 10_001:
-            prime_10001 = i
-            break
+        count_sum += i
         for j in range(i * i, MAX_SIZE, i):
             status[j] = False
 
-print(prime_10001)
+print(count_sum)
