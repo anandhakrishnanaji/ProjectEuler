2aa1bd9f93de3c91abe0c0a9c0b4ff366c6bb6dc
A Anandha Krishnan <creattech2000@gmail.com>
2024-04-28 10:41:54 +0530

    Added solution to problem 22

---
+++ problem_22.py
@@ -0,0 +1,14 @@
+BASE_ASCII = 64
+
+
+score = lambda pos, name: pos * sum(map(lambda x: ord(x) - BASE_ASCII, name))
+
+
+with open("data/0022_names.txt", "r") as f:
+    name_score_sum = sum(
+        map(
+            lambda tup: score(tup[0] + 1, tup[1].strip('"')),
+            enumerate(sorted(f.read().split(","))),
+        )
+    )
+    print(name_score_sum)
